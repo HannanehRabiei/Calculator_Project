a3fba08bfa62b5f3c69a51f2daba33bf779e157f
Hannaneh Rabiei <hannanehrabiei82@gmail.com>
2024-04-10 14:21:46 +0330

    Adding the SumOperator class and writing its logic

---
+++ src/main/java/MinusOperator.java
@@ -0,0 +1,13 @@
+import static java.lang.Double.parseDouble;
+
+public class MinusOperator {
+    public String menha(String number1, String number2) {
+
+        double d1 = parseDouble(number1);
+        double d2 = parseDouble(number2);
+        double result=d1-d2;
+
+
+        return String.valueOf(result);
+    }
+}
---
+++ src/test/java/MinusOperatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MinusOperatorTest {
+    @Test
+    void must_minus_two_number_together() {
+        MinusOperator minusOperator = new MinusOperator();
+
+        String theMinusOfTwoNumbers = minusOperator.menha("2", "1");
+
+        assertEquals("1" ,theMinusOfTwoNumbers);
+    }
+}
